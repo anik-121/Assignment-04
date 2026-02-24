14d19fe89f8adc9a1068a08e1f5343f3204d0c99
anik-121 <fahimmahmudanik@gmail.com>
2026-02-24 21:41:39 +0600

    added toggle

--- script.js
+++ script.js
@@ -8,3 +8,48 @@ let total = document.getElementById("total");
 
         const allCards = document.getElementById("allCards");
         const cardContainer = allCards.closest(".bg-white");
+
+
+        const interviewCards = document.createElement("section");
+        interviewCards.id = "interviewCards";
+        interviewCards.classList.add("hidden");
+
+        const rejectedCards = document.createElement("section");
+        rejectedCards.id = "rejectedCards";
+        rejectedCards.classList.add("hidden");
+
+        cardContainer.appendChild(interviewCards);
+        cardContainer.appendChild(rejectedCards);
+
+        function calculateCount() {
+            const allCount = allCards.querySelectorAll(".card").length;
+            const iCount = interviewCards.querySelectorAll(".card").length;
+            const rCount = rejectedCards.querySelectorAll(".card").length;
+            total.innerText = allCount + iCount + rCount;
+            interviewCount.innerText = iCount;
+            rejectedCount.innerText = rCount;
+            document.getElementById("jobCountLabel").innerText = (allCount + iCount + rCount) + " jobs";
+        }
+
+        calculateCount();
+
+
+         
+        function toggleStyle(id) {
+            [allFilterBtn, interviewFilterBtn, rejectedFilterBtn].forEach(btn => {
+                btn.classList.remove("bg-blue-400", "text-white");
+                btn.classList.add("text-slate-500");
+            });
+
+            const activeBtn = document.getElementById(id);
+            activeBtn.classList.add("bg-blue-400", "text-white");
+            activeBtn.classList.remove("text-slate-500");
+
+            allCards.classList.add("hidden");
+            interviewCards.classList.add("hidden");
+            rejectedCards.classList.add("hidden");
+
+            if (id === "all-filter-btn") allCards.classList.remove("hidden");
+            else if (id === "interview-filter-btn") interviewCards.classList.remove("hidden");
+            else if (id === "rejected-filter-btn") rejectedCards.classList.remove("hidden");
+        }
