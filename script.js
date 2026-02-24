ad6a8d11237aa795a76e158bf3eeff583ad3fb8f
anik-121 <fahimmahmudanik@gmail.com>
2026-02-24 21:23:25 +0600

    added some js

---
+++ script.js
@@ -0,0 +1,10 @@
+let total = document.getElementById("total");
+        let interviewCount = document.getElementById("interviewCount");
+        let rejectedCount = document.getElementById("rejectedCount");
+
+        const allFilterBtn = document.getElementById("all-filter-btn");
+        const interviewFilterBtn = document.getElementById("interview-filter-btn");
+        const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+
+        const allCards = document.getElementById("allCards");
+        const cardContainer = allCards.closest(".bg-white");
